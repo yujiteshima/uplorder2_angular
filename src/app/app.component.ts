816a740805e62aa5d858d4d7d088b71619b036ad
Yuji Teshima <yuji.teshima@gmail.com>
2020-09-03 22:35:42 +0900

    uploader2 first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'upload2';
+
+  constructor(private http: HttpClient) {
+  }
+
+  upload(files: Array<any>): void {
+    if (files.length <= 0) {
+      return;
+    }
+
+
+    const file = files[0];
+
+    const formDate = new FormData();
+
+    formDate.append('file', file, file.name);
+
+    this.http.post('http://localhost:8080/upload', formDate)
+      .subscribe(
+        data => console.log(data),
+        error => console.log(error)
+      );
+  }
 }
